5cd1a4b0e0a34bde50dc9b5f0fe2a9f9572e1bd8
Stanley van Heusden <stanley@stanleyvanheusden.nl>
2018-09-16 16:56:06 +0200

    Initial Release

---
+++ header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+    <head>
+        <meta charset="<?php bloginfo( 'charset' ); ?>" />
+        <title><?php wp_title(); ?></title>
+        <link rel="profile" href="http://gmpg.org/xfn/11" />
+        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+        <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
+        <?php wp_head(); ?>
+    </head>
